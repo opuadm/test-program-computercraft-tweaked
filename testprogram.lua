fe05c17359f8e5ae87b423076891352dbdbcf67d
opuadm <79022132+opuadm@users.noreply.github.com>
2024-04-10 16:49:04 +0200

    Update testprogram.lua

--- testprogram.lua
+++ testprogram.lua
@@ -1,13 +1,16 @@
 function patience(seconds)
     local start = os.time()
-    repeat until os.time() > start + seconds
+    while os.time() - start < seconds do
+        os.execute("sleep 0.01")
+    end
 end
+
 
 if _G == nil then
     _G = {}
 end
 
-_G.VerTest = "Main 0.1.0a"
 
+_G.VerTest = "Main 0.1.0a"
 io.write("Current version of this test program is: ", _G.VerTest)
 patience(2.5)
