e4d92a2f70d3d80573bdad2116729f89aad0b642
opuadm <79022132+opuadm@users.noreply.github.com>
2024-04-10 16:57:05 +0200

    Update testprogram.lua

--- testprogram.lua
+++ testprogram.lua
@@ -1,6 +1,9 @@
+local socket = require 'socket'
+
 if _G == nil then
     _G = {}
 end
 
 _G.VerTest = "Main 0.1.0a"
 io.write("Current version of this test program is: ", _G.VerTest)
+socket.sleep(2)
