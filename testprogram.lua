af4590c694af4b2d5afa6e26b0c0b1ed7ed70a2f
opuadm <79022132+opuadm@users.noreply.github.com>
2024-04-10 17:37:18 +0200

    changes

--- testprogram.lua
+++ testprogram.lua
@@ -2,14 +2,29 @@ if _G == nil then
     _G = {}
 end
 
-function wait(seconds)
-    local start = os.clock()
-    while os.clock() - start < seconds do end
+function clearConsole()
+    -- Clear the console by printing multiple newline characters
+    term.clear()
+    term.setCursorPos(1, 1)  -- Move cursor to the top-left corner
 end
 
+function moveCursorDown()
+    -- Move the cursor down by one line
+    local _, y = term.getCursorPos()
+    term.setCursorPos(1, y + 1)
+end
+
+function wait(seconds)
+    os.sleep(seconds)
+end
+
+clearConsole()  -- Clear the console before printing anything
 _G.VerTest = "Main 0.1.0a"
-io.write("Current version of this test program is: ", _G.VerTest)
+print("Current version of this test program is: " .. _G.VerTest)
 wait(2)
-io.write("Type Ur Username u want to have in this app")
-local usrname = io.read()
-io.write("Ok! So ur username is ", usrname)
+
+print("Type your username you want to have in this app:")
+local username = read()  -- Using 'read()' to get user input
+print("Ok! So your username is " .. username)
+
+moveCursorDown()  -- Move the cursor down by one line
