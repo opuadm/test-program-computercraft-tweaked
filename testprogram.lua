08e4a4c00bbf7f87e978b0c69bdae8d34c87e375
opuadm <79022132+opuadm@users.noreply.github.com>
2024-04-10 16:59:17 +0200

    Update testprogram.lua

--- testprogram.lua
+++ testprogram.lua
@@ -1,4 +1,6 @@
-local socket = require 'socket'
+function sleep(n)
+  if n > 0 then os.execute("ping -n " .. tonumber(n+1) .. " localhost > NUL") end
+end
 
 if _G == nil then
     _G = {}
@@ -6,4 +8,4 @@ end
 
 _G.VerTest = "Main 0.1.0a"
 io.write("Current version of this test program is: ", _G.VerTest)
-socket.sleep(2)
+sleep(2)
