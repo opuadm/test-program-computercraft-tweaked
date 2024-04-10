609f997e5a9b3092c7df15f131e91a3eba03316c
opuadm <79022132+opuadm@users.noreply.github.com>
2024-04-10 17:15:36 +0200

    Update testprogram.lua

--- testprogram.lua
+++ testprogram.lua
@@ -7,7 +7,6 @@ function wait(seconds)
     while os.clock() - start < seconds do end
 end
 
-clearLine()
 _G.VerTest = "Main 0.1.0a"
 io.write("Current version of this test program is: ", _G.VerTest)
 wait(2)
