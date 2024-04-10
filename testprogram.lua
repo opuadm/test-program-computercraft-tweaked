815c57ee198e2ef31533fccc3529b1afb2820fcd
opuadm <79022132+opuadm@users.noreply.github.com>
2024-04-10 17:05:32 +0200

    Update testprogram.lua

--- testprogram.lua
+++ testprogram.lua
@@ -1,6 +1,12 @@
+function clearLine()
+    io.write("\27[2K")  -- ANSI escape code to clear the current line
+    io.write("\27[0G")  -- ANSI escape code to move the cursor to the beginning of the line
+end
+
 if _G == nil then
     _G = {}
 end
 
+clearLine()
 _G.VerTest = "Main 0.1.0a"
 io.write("Current version of this test program is: ", _G.VerTest)
