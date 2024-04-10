0d438d6cfb4af8a70d61a3b5072f086423193f59
opuadm <79022132+opuadm@users.noreply.github.com>
2024-04-10 17:19:44 +0200

    idkclears

--- testprogram.lua
+++ testprogram.lua
@@ -2,11 +2,17 @@ if _G == nil then
     _G = {}
 end
 
+function clearConsole()
+    -- ANSI escape code to clear the screen
+    io.write("\27[2J\27[1;1H")
+end
+
 function wait(seconds)
     local start = os.clock()
     while os.clock() - start < seconds do end
 end
 
+clearConsole()  -- Clear the console before printing anything
 _G.VerTest = "Main 0.1.0a"
 io.write("Current version of this test program is: ", _G.VerTest)
 wait(2)
