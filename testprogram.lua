cf38a21b975b81211af80956a3493c546a611361
opuadm <79022132+opuadm@users.noreply.github.com>
2024-04-10 17:10:55 +0200

    Update testprogram.lua

--- testprogram.lua
+++ testprogram.lua
@@ -2,9 +2,12 @@ if _G == nil then
     _G = {}
 end
 
+function wait(seconds)
+    local start = os.clock()
+    while os.clock() - start < seconds do end
+end
+
 clearLine()
 _G.VerTest = "Main 0.1.0a"
 io.write("Current version of this test program is: ", _G.VerTest)
-for i = 1, 10 do
-    print("Silence...")
-end
+wait(2)
